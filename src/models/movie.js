77ace4ad62175fb05e3d1e770ae2f4f9cdc3919d
SHAIK SURAZ <shaik.suraz50@gmail.com>
2025-02-13 21:30:27 +0530

    changed schema

--- src/models/movie.js
+++ src/models/movie.js
@@ -1,6 +1,93 @@
+// const mongoose = require("mongoose");
+// const { Schema } = mongoose;
+
+// const { genreData } = require("../utils/data");
+
+// const MovieSchema = new Schema(
+//   {
+//     title: {
+//       type: String,
+//       trim: true,
+//       required: true,
+//     },
+//     storyLine: {
+//       type: String,
+//       trim: true,
+//       required: true,
+//     },
+//     director: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Actor",
+//     },
+//     releaseDate: {
+//       type: Date,
+//       required: true,
+//     },
+//     status: {
+//       type: String,
+//       required: true,
+//       enum: ["public", "private"], // if public , display to public otherwise only admin
+//     },
+//     type: {
+//       type: String,
+//       required: true,
+//     },
+//     genres: {
+//       type: [String],
+//       required: true,
+//       enum: genreData,
+//     },
+//     tags: {
+//       type: [String],
+//       required: true,
+//     },
+//     cast: [
+//       {
+//         actor: { type: mongoose.Schema.Types.ObjectId, ref: "Actor" },
+//         roleAs: String,
+//         leadActor: Boolean,
+//       },
+//     ],
+//     writers: [
+//       {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "Actor",
+//       },
+//     ],
+//     poster: {
+//       type: Object,
+//       url: { type: String, required: true },
+//       public_id: { type: String, required: true },
+//       responsiveImages: [URL],
+//     },
+//     trailer: {
+//       type: Object,
+//       url: { type: String, required: true },
+//       public_id: { type: String, required: true },
+//       required: true,
+//     },
+//     reviews: [
+//       {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "Review",
+//       },
+//     ],
+//     language: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// // MovieSchema.index({ title: "text" });
+// MovieSchema.index({ title: "text", storyLine: "text" }, { default_language: "none" });
+
+
+// const Movie = mongoose.model("Movie", MovieSchema);
+// module.exports = Movie;
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-
 const { genreData } = require("../utils/data");
 
 const MovieSchema = new Schema(
@@ -26,7 +113,7 @@ const MovieSchema = new Schema(
     status: {
       type: String,
       required: true,
-      enum: ["public", "private"], // if public , display to public otherwise only admin
+      enum: ["public", "private"],
     },
     type: {
       type: String,
@@ -75,12 +162,28 @@ const MovieSchema = new Schema(
     language: {
       type: String,
       required: true,
+      // You can set a default if you want:
+      // default: "multilingual"
     },
   },
   { timestamps: true }
 );
 
-MovieSchema.index({ title: "text" });
+// Update the index to ignore the "language" field for text search
+// MovieSchema.index(
+//   { title: "text", storyLine: "text" },
+//   { default_language: "none", language_override: "dummy" }
+// );
+// Updated text index: ignore the "language" field from documents by overriding it with "dummy"
+// MovieSchema.index(
+//   { title: "text", storyLine: "text" },
+//   { default_language: "none", language_override: "dummy" }
+// );
+MovieSchema.index(
+  { title: "text", storyLine: "text" },
+  { default_language: "none", language_override: "dummy" }
+);
+
 
 const Movie = mongoose.model("Movie", MovieSchema);
 module.exports = Movie;
